d48a536f27d41225ab5c9fafac0c9967bf89896d
hadi.tayebi <tayebi1426@gmail.com>
2023-08-20 14:47:28 +0330

    config project

--- spring-examples/spring-security-examples/basic-authentication/src/test/java/org/examples/spring/security/HttpBasicSecurityTest.java
+++ spring-examples/spring-security-examples/basic-authentication/src/test/java/org/examples/spring/security/HttpBasicSecurityTest.java
@@ -11,20 +11,17 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.net.URI;
 
-//@SpringBootTest
-//@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @PropertySource({"classpath:application.yml"})
-//@ContextConfiguration(classes = {ApplicationContextTest.class, PersistenceContextTest.class})
-//@SetEnvironmentVariable(key = "spring_main_allow-bean-definition-overriding", value = "true")
 @WebMvcTest(controllers = UserController.class)
-public class SecurityHttpBasicApplicationTests {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityHttpBasicApplicationTests.class);
+public class HttpBasicSecurityTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpBasicSecurityTest.class);
     @Autowired
     MockMvc mockMvc;
 
@@ -36,14 +33,16 @@ public class SecurityHttpBasicApplicationTests {
         final HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setBasicAuth(username, password);
         //when
-        this.mockMvc.perform(MockMvcRequestBuilders.get(URI.create("/user"))
+        final ResultActions resultActions = this.mockMvc.perform(MockMvcRequestBuilders.get(URI.create("/user"))
                         .headers(httpHeaders))
-                //then
-                .andDo(result -> LOGGER.info("server response >> {}  ", result.getResponse().getContentAsString()))
+                .andDo(result -> LOGGER.info("server response >> {}  ", result.getResponse().getContentAsString()));
+        //then
+        resultActions
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=UTF-8"))
                 .andExpect(MockMvcResultMatchers.content().string(IsEqualIgnoringCase.equalToIgnoringCase(username)));
     }
+
     @Test
     public void failedAuthentication() throws Exception {
         //given
@@ -52,11 +51,11 @@ public class SecurityHttpBasicApplicationTests {
         final HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setBasicAuth(username, password);
         //when
-        this.mockMvc.perform(MockMvcRequestBuilders.get(URI.create("/user"))
+        final ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(URI.create("/user"))
                         .headers(httpHeaders))
-                //then
-                .andDo(result -> LOGGER.info("server response >> {}  ", result.getResponse().getContentAsString()))
-                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+                .andDo(result -> LOGGER.info("server response >> {}  ", result.getResponse().getContentAsString()));
+        //then
+        resultActions.andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 
 }
