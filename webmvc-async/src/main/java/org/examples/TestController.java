c8682768f2179165ba4a67cecc2ae4828bdbc613
h.tayebi <h.tayebi@demisco.com>
2024-08-14 16:41:29 +0330

    mongodb example

--- webmvc-async/src/main/java/org/examples/TestController.java
+++ webmvc-async/src/main/java/org/examples/TestController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 
 @RestController
 public class TestController {
@@ -16,14 +15,19 @@ public class TestController {
     @GetMapping("/test")
     @Async("mvcTaskExecutor")
     public CompletableFuture<String> hello(ServletWebRequest webRequest) {
-        Assert.isTrue(webRequest.getRequest().isAsyncStarted(),"webRequest is not async");
         String mvcThreadName = Thread.currentThread().getName();
         Assert.isTrue(mvcThreadName.startsWith("mvc-task"), "miss configuration");
         System.out.printf("TestController.hello [threadName=%s] \n", mvcThreadName);
         return CompletableFuture.supplyAsync(() -> {
-            System.out.printf("CompletableFuture.supplyAsync [threadName=%s] \n", Thread.currentThread().getName());
-            //do anything
-            return "hi, dear user";
+            try {
+                Thread.sleep(3_000);
+                System.out.printf("CompletableFuture.supplyAsync [threadName=%s] \n", Thread.currentThread().getName());
+                Assert.isTrue(webRequest.getRequest().isAsyncStarted(), "webRequest is not async");
+                //do anything
+                return "hi, dear user";
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         });
     }
 }
